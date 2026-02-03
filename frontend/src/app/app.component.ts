1c6d842d74b6cab1d3d718351737c7b5ee9aa0b1
Hichemch-stack <hichem.chebbi@sesame.com.tn>
2026-02-03 20:15:49 +0000

    fix

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { UserComponent } from './components/user/user.component';
+import { Component, OnInit } from '@angular/core';
+import { UserService, User } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [UserComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Application DevOps</h1>
+    <h2>Liste des utilisateurs</h2>
+    <ul>
+      <li *ngFor="let user of users">
+        {{ user.name }} - {{ user.email }}
+      </li>
+    </ul>
+    <p *ngIf="users.length === 0">Aucun utilisateur trouvé.</p>
+  `,
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]  // facultatif si déjà "providedIn: root"
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe({
+      next: data => this.users = data,
+      error: err => console.error('Erreur API:', err)
+    });
+  }
+}
 
